1d9907b5878b8278421eb3f344411884e0b900c2
白浩然 <baihaoran9638@gmail.com>
2019-10-06 20:14:26 +0800

    每500ms 主动触发一次事件，添加三个图标

--- app/src/main/java/com/daoshengwanwu/android/service/AuxiliaryService.java
+++ app/src/main/java/com/daoshengwanwu/android/service/AuxiliaryService.java
@@ -3,6 +3,9 @@ package com.daoshengwanwu.android.service;
 
 import android.accessibilityservice.AccessibilityService;
 import android.content.Intent;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
 import android.widget.Toast;
@@ -13,18 +16,49 @@ import com.daoshengwanwu.android.util.SingleSubThreadUtil;
 
 
 public class AuxiliaryService extends AccessibilityService {
+    private static final int WHAT_BREATH_INTERVAL = 58363;
+    private Handler mMainHandler = null;
+
+    private AccessibilityEvent mLastEvent = null;
+
+
     @Override
     protected void onServiceConnected() {
         super.onServiceConnected();
 
         SingleSubThreadUtil.showToast(this, "成功启动 " +
                 getResources().getString(R.string.app_name) + ".", Toast.LENGTH_SHORT);
+
+        mMainHandler = new Handler(Looper.getMainLooper()) {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+
+                switch (msg.what) {
+                    case WHAT_BREATH_INTERVAL: {
+                        if (mLastEvent != null) {
+                            onAccessibilityEvent(mLastEvent);
+                        }
+
+                        mMainHandler.sendEmptyMessageDelayed(WHAT_BREATH_INTERVAL, 500);
+                    } break;
+
+                    default: break;
+                }
+            }
+        };
+
+        mMainHandler.sendEmptyMessageDelayed(WHAT_BREATH_INTERVAL, 500);
     }
 
     @Override
     public boolean onUnbind(Intent intent) {
         Toast.makeText(this, getResources().getString(R.string.app_name) +
                 "已关闭，如需再次使用请在设置中重新开启本插件.", Toast.LENGTH_SHORT).show();
+
+        if (mMainHandler != null) {
+            mMainHandler.removeMessages(WHAT_BREATH_INTERVAL);
+        }
 
         return super.onUnbind(intent);
     }
@@ -36,16 +70,16 @@ public class AuxiliaryService extends AccessibilityService {
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
-        Task curActivatedTask = getCurActivatedTask();
-
-        if (curActivatedTask == null) {
-            return;
-        }
-
         if (event.getEventType() != AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED &&
             event.getEventType() != AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED &&
             event.getEventType() != AccessibilityEvent.TYPE_VIEW_CLICKED) {
 
+            return;
+        }
+        mLastEvent = AccessibilityEvent.obtain(event);
+
+        Task curActivatedTask = getCurActivatedTask();
+        if (curActivatedTask == null) {
             return;
         }
 
@@ -55,6 +89,7 @@ public class AuxiliaryService extends AccessibilityService {
         if (source == null || rootInfo == null) {
             return;
         }
+
         curActivatedTask.execute(rootInfo);
     }
 
