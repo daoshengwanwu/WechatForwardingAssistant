a632ad525b91ff317486fc380fc73082b96e328a
白浩然 <baihaoran.happyday@bytedance.com>
2021-05-06 15:21:57 +0800

    adjust AuxiliaryService

--- app/src/main/java/com/daoshengwanwu/android/service/AuxiliaryService.java
+++ app/src/main/java/com/daoshengwanwu/android/service/AuxiliaryService.java
@@ -18,9 +18,27 @@ import com.daoshengwanwu.android.util.SingleSubThreadUtil;
 
 public class AuxiliaryService extends AccessibilityService {
     private static final int WHAT_BREATH_INTERVAL = 58363;
-    private Handler mMainHandler = null;
+
 
     private AccessibilityEvent mLastEvent = null;
+    private final Handler mMainHandler = new Handler(Looper.getMainLooper()) {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case WHAT_BREATH_INTERVAL: {
+                    final AccessibilityEvent lastEvent = mLastEvent;
+
+                    if (lastEvent != null) {
+                        onAccessibilityEvent(lastEvent);
+                    }
+
+                    mMainHandler.sendEmptyMessageDelayed(WHAT_BREATH_INTERVAL, 1000);
+                } break;
+
+                default: break;
+            }
+        }
+    };
 
 
     @Override
@@ -30,25 +48,6 @@ public class AuxiliaryService extends AccessibilityService {
         SingleSubThreadUtil.showToast(this, "成功启动 " +
                 getResources().getString(R.string.app_name) + ".", Toast.LENGTH_SHORT);
 
-        mMainHandler = new Handler(Looper.getMainLooper()) {
-            @Override
-            public void handleMessage(Message msg) {
-                super.handleMessage(msg);
-
-                switch (msg.what) {
-                    case WHAT_BREATH_INTERVAL: {
-                        if (mLastEvent != null) {
-                            onAccessibilityEvent(mLastEvent);
-                        }
-
-                        mMainHandler.sendEmptyMessageDelayed(WHAT_BREATH_INTERVAL, 500);
-                    } break;
-
-                    default: break;
-                }
-            }
-        };
-
         mMainHandler.sendEmptyMessageDelayed(WHAT_BREATH_INTERVAL, 500);
     }
 
@@ -57,9 +56,7 @@ public class AuxiliaryService extends AccessibilityService {
         Toast.makeText(this, getResources().getString(R.string.app_name) +
                 "已关闭，如需再次使用请在设置中重新开启本插件.", Toast.LENGTH_SHORT).show();
 
-        if (mMainHandler != null) {
-            mMainHandler.removeMessages(WHAT_BREATH_INTERVAL);
-        }
+        mMainHandler.removeMessages(WHAT_BREATH_INTERVAL);
 
         return super.onUnbind(intent);
     }
@@ -77,14 +74,15 @@ public class AuxiliaryService extends AccessibilityService {
 
             return;
         }
+
         mLastEvent = AccessibilityEvent.obtain(event);
 
-        Task curActivatedTask = getCurActivatedTask();
+        final Task curActivatedTask = getCurActivatedTask();
         if (curActivatedTask == null) {
             return;
         }
 
-        AccessibilityNodeInfo rootInfo = getRootInActiveWindow();
+        final AccessibilityNodeInfo rootInfo = getRootInActiveWindow();
 
         if (rootInfo == null) {
             return;
